3c932561c081d53231f5e089ecee61d057c755aa
Mathieu Alorent <github@kumy.net>
2017-03-31 16:43:34 +0000

    Insert avatar picture

--- gkm/gkavatar.php
+++ gkm/gkavatar.php
@@ -7,9 +7,9 @@ $query = null;
 
 // parse gkid
 if (isset($_GET['gkid'])) {
-  $query = query($session, 'select-by-gkid.xq');
-  $query->bind('gkid', $_GET['gkid'], 'xs:string');
-  $query->bind('details', '-details', 'xs:string');
+   $query = query($session, 'select-by-gkid.xq');
+   $query->bind('gkid', $_GET['gkid'], 'xs:string');
+   $query->bind('details', '-details', 'xs:string');
 }
 $xmlstr = execute($session, $query);
 
@@ -17,27 +17,35 @@ $geokrety = new SimpleXMLElement($xmlstr);
 
 $msgs = [];
 foreach($geokrety->geokrety->geokret as $gk) {
- $msgs[] = $gk['id'] .' '.$gk->name." (".$gk->places."): ".$gk->distancetraveled."km";
+   $msgs[] = $gk['id'];
+   $msgs[] = $gk->name;
+   $msgs[] = 'Places visited: '.$gk->places;
+   $msgs[] = 'Has travelled: '.$gk->distancetraveled.'km';
 }
 
 $nbMsg = count($msgs);
 
 header ("Content-type: image/png");
-$image = imagecreate(400,20 + 15 * $nbMsg);
+$image = imagecreatetruecolor(300,300);
 
-$blanc = imagecolorallocate($image, 255, 255, 255);
-
+$white = imagecolorallocate($image, 255, 255, 255);
 $orange = imagecolorallocate($image, 255, 128, 0);
-$bleu = imagecolorallocate($image, 0, 0, 255);
-$bleuclair = imagecolorallocate($image, 156, 227, 254);
-$noir = imagecolorallocate($image, 0, 0, 0);
+$blue = imagecolorallocate($image, 0, 0, 255);
+$lightblue = imagecolorallocate($image, 156, 227, 254);
+$black = imagecolorallocate($image, 0, 0, 0);
 $font = getcwd() . '/DejaVuSansMono.ttf'; // '/DejaVuSerif.ttf'; // 'arial.ttf';
 
-$textColor = $noir;
+imagefill($image, 0, 0, $white);
+
+if (isset($gk->image) && !empty($gk->image)) {
+   $avatarpicture = imagecreatefromjpeg('https://api.geokretymap.org/gkimage/'.$gk->image);
+   imagecopy($image, $avatarpicture, 100, 100, 0, 0, 100, 100);
+}
+
+$textColor = $black;
 
 for ($i=0;$i<$nbMsg;$i++) {
-  // imagestring($image, 4, 10, 10 + ($i*15), $msgs[$i], $textColor);
-  imagettftext($image, 10, 0, 10, 20 + ($i*15), $textColor, $font, $msgs[$i]);
+   imagettftext($image, 10, 0, 10, 20 + ($i*15), $textColor, $font, $msgs[$i]);
 }
 
 imagepng($image);
