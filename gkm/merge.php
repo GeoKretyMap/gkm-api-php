9f94021a99976aa09b0e0f71c05db904dae80c13
Mathieu Alorent <github@kumy.net>
2017-06-26 21:29:02 +0200

    Disable prometeus things

--- gkm/merge.php
+++ gkm/merge.php
@@ -1,23 +1,23 @@
 <?php
 
-require_once dirname(__FILE__) . '/lib/Prometheus/Client.php';
+#require_once dirname(__FILE__) . '/lib/Prometheus/Client.php';
 include_once('lib/gkm.php');
 
-$client = new Prometheus\Client(['base_uri' => 'http://prometheus:9091/metrics/job/']);
-
-$counter_call = $client->newCounter([
-        'namespace' => 'gkm',
-        'subsystem' => 'php',
-        'name' => 'merge_calls_counter',
-        'help' => 'Measure call to the merge page',
-    ]);
-
-$counter_update = $client->newCounter([
-        'namespace' => 'gkm',
-        'subsystem' => 'php',
-        'name' => 'merge_counter',
-        'help' => 'Measure update rate',
-    ]);
+#$client = new Prometheus\Client(['base_uri' => 'http://prometheus:9091/metrics/job/']);
+#
+#$counter_call = $client->newCounter([
+#        'namespace' => 'gkm',
+#        'subsystem' => 'php',
+#        'name' => 'merge_calls_counter',
+#        'help' => 'Measure call to the merge page',
+#    ]);
+#
+#$counter_update = $client->newCounter([
+#        'namespace' => 'gkm',
+#        'subsystem' => 'php',
+#        'name' => 'merge_counter',
+#        'help' => 'Measure update rate',
+#    ]);
 
 $session = session();
 $query = null;
@@ -25,12 +25,12 @@ $details = isset($_GET['details']);
 
 if ($details) {
   $query = query($session, 'merge-details.xq');
-  $counter_call->increment( [ 'details' => $details ], 1 );
+  #$counter_call->increment( [ 'details' => $details ], 1 );
 } else {
   $query = query($session, 'merge.xq');
-  $counter_call->increment( [ 'details' => $details ], 1 );
+  #$counter_call->increment( [ 'details' => $details ], 1 );
 }
 
 $value = renderValue($session, $query);
-$counter_update->increment( [ 'details' => $details ], $value );
-$client->pushMetrics( "gkm", "merge" );
+#$counter_update->increment( [ 'details' => $details ], $value );
+#$client->pushMetrics( "gkm", "merge" );
